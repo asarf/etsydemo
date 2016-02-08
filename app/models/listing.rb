3485479cbf4ddf3d59736c532ecb8addce1e2a2c
sarfaraz ahmed <sahmed@essendant.com>
2016-02-08 16:11:22 +0000

    Added figaro gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ActiveRecord::Base
-    has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
+    has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "/images/:style/missing"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
